f091ae97efc88dd4ee9f93ff731c3f7b2f8f6ce8
Vetaman999 <68401851+Vetaman999@users.noreply.github.com>
2021-10-02 00:27:21 -0500

    added Mapping files

---
+++ PeruStars_2/PeruStars_2/Mapping/ModelToResourceProfile.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using PeruStars_2.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace PeruStars_2.Mapping
+{
+    public class ModelToResourceProfile : Profile
+    {
+        public ModelToResourceProfile()
+        {
+            CreateMap<Artist, ArtistResource>();
+            CreateMap<Hobbyist, HobbyistResource>();
+            CreateMap<Person, PersonResource>();
+            CreateMap<Event, EventResource>();
+            CreateMap<ClaimTicket, ClaimTicketResource>();
+            CreateMap<EventAssistance, EventAssistanceResource>();
+            CreateMap<Artwork, ArtworkResource>();
+            CreateMap<Specialty, SpecialtyResource>();
+
+        }
+    }
+}
---
+++ PeruStars_2/PeruStars_2/Mapping/ResourceToModelProfile.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using PeruStars_2.Domain.Models;
+using PeruStars_2.Domain.Services.Communications;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PeruStars_2.Mapping
+{
+    public class ResourceToModelProfile : Profile
+    {
+        public ResourceToModelProfile()
+        {
+            CreateMap<SaveArtistResource, Artist>();
+            CreateMap<SavePersonResource, Person>();
+            CreateMap<SaveArtworkResource, Artwork>();
+            CreateMap<SaveEventResource, Event>();
+            CreateMap<SaveClaimTicketResource, ClaimTicket>();
+            CreateMap<SaveEventAssistanceResource, EventAssistance>();
+            CreateMap<SaveHobbyistResource, Hobbyist>();
+            CreateMap<RegisterRequest, User>();
+        }
+    }
+}
