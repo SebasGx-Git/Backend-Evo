29b5a7b7ff487b55f8a62831bc4aa50a296f4d44
SebasGx <sgabad@hotmail.com>
2021-10-02 12:01:40 -0500

    Added Resource and Extensions

---
+++ PeruStars_2/PeruStars_2/Extensions/ModelBuilderExtensions.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PeruStars_2.Extensions
+{
+    public class ModelBuilderExtensions
+    {
+    }
+}
